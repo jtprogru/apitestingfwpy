08a79d6240c00aae220e4fe4ad909908e1e7ad2c
Michael Savin <jtprogru@gmail.com>
2024-08-02 00:49:56 +0300

    Create test for Get object

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,7 @@
 import pytest
 
 from endpoints.create_object import CreateObject
+from endpoints.get_object import GetObject
 
 
 def test_create_object(payload_for_create):
@@ -8,3 +9,10 @@ def test_create_object(payload_for_create):
     create_object_endpoint.create_new_object(payload_for_create)
     create_object_endpoint.check_status_code_is_200ok()
     create_object_endpoint.check_name(payload_for_create["name"])
+
+
+def test_get_object(object_id):
+    get_object_endpoint = GetObject()
+    get_object_endpoint.get_object_by_id(object_id)
+    get_object_endpoint.check_status_code_is_200ok()
+    get_object_endpoint.check_object_id(object_id)
